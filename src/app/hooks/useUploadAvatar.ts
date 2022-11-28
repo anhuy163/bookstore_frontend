5a4c5aba3a12f5e1eabcfd3acdde91214103b790
DESKTOP-E8F9381\admin <bestriven.weasky@gmail.com>
2022-11-28 18:57:58 +0700

    done

--- src/app/hooks/useUploadAvatar.ts
+++ src/app/hooks/useUploadAvatar.ts
@@ -6,14 +6,13 @@ const config = {
   region: "us-east-1",
   accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY,
   secretAccessKey: process.env.NEXT_PUBLIC_SECRET_KEY,
-  dirName: "avatar",
-  s3Url: "https://bookstoreimages.s3.amazonaws.com",
+  s3Url: "https://bookstoreimages.s3.amazonaws.com/",
 };
 
 const useUploadAvatar = () => {
   const ReactS3Client = new S3(config);
   const uploadAvatar = async (file) => {
-    // console.log(file.name);
+    console.log(file.name);
 
     try {
       const res = await ReactS3Client.uploadFile(
